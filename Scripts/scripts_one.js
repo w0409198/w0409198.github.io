f2c9628023c11abe5bd7401dc401ae9f91bbb64e
daltonparker <W0409198@nscc.ca>
2017-12-06 14:13:29 -0400

    Saved

--- Scripts/scripts_one.js
+++ Scripts/scripts_one.js
@@ -11,6 +11,18 @@ function show_web_portfolio() {
 
 }
 
+function show_web_portfolio_1() {
+    var target_element_1 = document.getElementById("work_samples_content");
+    target_element_1.innerHTML = 'NEW INNEr TEXT TESTETSTET' +
+        '<p><button onClick="history.go(0)" class="button">Go Back</button></p>';
+
+    var target_element_2 = document.getElementById("work_samples_head");
+    target_element_2.innerHTML = 'TEST';
+
+
+}
+
 
 
 document.getElementById("web_portfolio").addEventListener("click", show_web_portfolio);
+document.getElementById("test").addEventListener("click", show_web_portfolio_1);
